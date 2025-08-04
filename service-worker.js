b0e89726efa20e1469f6325a99ea4800cb28d7a6
rikhard7707 <ricardoataide171@gmail.com>
2025-08-04 19:42:27 -0300

    Add files via upload
    A ideia é poder através do aplicativo está utilizando os hardware como câmera caixa de som microfone para estar usando em aplicativos.

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('gravador1-cache').then(function(cache) {
+      return cache.addAll(['./','./index.html','./manifest.json','./icon.png']);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response){
+    return response || fetch(event.request);
+  }));
+});
